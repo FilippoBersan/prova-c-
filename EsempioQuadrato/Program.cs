927df91605560cd5f32d91d4e70b4ec17b028460
FilippoBersan <filippobersan99@gmail.com>
2024-06-12 16:25:29 +0200

    first commit

---
+++ EsempioQuadrato/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EsempioQuadrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci un numero:");
+            string input = Console.ReadLine();
+
+            // Convertire l'input in un numero intero
+            if (int.TryParse(input, out int numero))
+            {
+                int quadrato = numero * numero;
+                Console.WriteLine($"Il quadrato di {numero} è {quadrato}.");
+            }
+            else
+            {
+                Console.WriteLine("Per favore, inserisci un numero valido.");
+            }
+        }
+    }
+}
